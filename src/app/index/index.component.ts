54de3e7108da126fb0ff18dd25c3b95ab3f783a8
KeremAyseli <keremayseli@outlook.com>
2021-09-08 02:22:48 +0300

    first commit

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -3,9 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
-  styleUrls: ['./index.component.css','../../assets/css/styles.css',
-  'https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic',
-  'https://fonts.googleapis.com/css?family=Montserrat:400,700']
+  styleUrls: ['./index.component.css','../../assets/css/styles.css']
 })
 export class IndexComponent implements OnInit {
 
